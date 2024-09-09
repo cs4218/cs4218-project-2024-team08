d796e414699513765898a818513f1cd10c519bbc
jundatan <jundatan@hotmail.com>
2024-09-09 17:16:42 +0800

    Testing CI

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <div className="footer">
-        <h4 className="text-center">All Rights Reserved &copy; TestingComp</h4>
+        <h4 className="text-center">All Rights Reserved &copy; Testing</h4>
         <p className="text-center mt-3">
         <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
         <Link to="/policy">Privacy Policy</Link>
