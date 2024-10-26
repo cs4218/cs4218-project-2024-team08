72af3ead7bfadd371efb1b03e2142c3dab9a541a
Koh Chen Huang, Samuel <88918588+Samsation@users.noreply.github.com>
2024-10-26 22:35:01 +0800

    Dummy commit

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -71,7 +71,6 @@ module.exports = defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    timeout: 2 * 60 * 1000,
     command: 'npm run dev',
     port: 3000,
     reuseExistingServer: !process.env.CI,
